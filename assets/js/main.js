e0282e20fd71d0976810398786d63a8e9407dc63
hailey <yanghyeri73@gmail.com>
2022-07-04 11:29:10 +0900

    admin chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,15 @@ function liveChatClose(){
     const liveChatWrap = document.querySelector('.live-chatting-wrap')
     liveChatWrap.classList.remove('on')
 }
+function searchExpand(event){
+    const target = event.currentTarget
+    const targetSearch = target.parentNode
+    const targetSearchImg = targetSearch.querySelector('img')
+    targetSearch.classList.toggle('on')
+    if(targetSearch.classList.contains('on')){
+        targetSearchImg.src = '/assets/img/icon-search.png'
+    }else{
+        targetSearchImg.src = '/assets/img/icon-search-grey.png'
+    }
+
+}
